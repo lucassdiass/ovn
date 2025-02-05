583d4156fd4e6a96cf996d9dd99d02635261ee12
Lucas Vargas Dias <lucas.vdias@luizalabs.com>
2025-02-05 10:26:26 -0300

    ovn-ic: Add support for route prefix filter
    This commit adds the ability for ovn-ic to filter routes between VPCs.
    In use cases where the user is using vpc-peering(route tag) handling
    multiple points of redistribution, when we connect more than one TS in
    the same Logical Router it is essential that we have control over which
    routes will be filtered or advertised by transit switch.
    
    Signed-off-by: Lucas Vargas Dias <lucas.vdias@luizalabs.com>

--- ic/ovn-ic.c
+++ ic/ovn-ic.c
@@ -1070,6 +1070,86 @@ get_nexthop_from_lport_addresses(bool is_v4,
 }
 
 static bool
+prefix_is_filtered(struct in6_addr *prefix,
+                   unsigned int plen,
+                   const struct nbrec_logical_router *nb_lr,
+                   const struct nbrec_logical_router_port *ts_lrp,
+                   bool is_advertisement)
+{
+    struct ds filter_list = DS_EMPTY_INITIALIZER;
+    const char *filter_direction = is_advertisement ? "ic-route-filter-adv" :\
+        "ic-route-filter-learn";
+    if (ts_lrp) {
+        const char *lrp_route_filter = smap_get(&ts_lrp->options,
+                                         filter_direction);
+        if (lrp_route_filter) {
+            ds_put_format(&filter_list, "%s,", lrp_route_filter);
+        }
+    }
+    const char *lr_route_filter = smap_get(&nb_lr->options,
+                                           filter_direction);
+    if (lr_route_filter) {
+        ds_put_format(&filter_list, "%s,", lr_route_filter);
+    }
+
+    if (!filter_list.length) {
+        ds_destroy(&filter_list);
+        return true;
+    }
+
+    struct in6_addr bl_prefix;
+    unsigned int bl_plen;
+    char *cur, *next, *start;
+    next = start = xstrdup(ds_cstr(&filter_list));
+    bool matched = false;
+    while ((cur = strsep(&next, ",")) && *cur) {
+        if (!ip46_parse_cidr(cur, &bl_prefix, &bl_plen)) {
+            static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
+            VLOG_WARN_RL(&rl, "Bad format in logical router/logical router"
+                         "port options: %s: %s. CIDR expected.",
+                         filter_direction, cur);
+            matched = true;
+            break;
+        }
+
+        if (IN6_IS_ADDR_V4MAPPED(&bl_prefix) != IN6_IS_ADDR_V4MAPPED(prefix)) {
+            continue;
+        }
+
+        /* 192.168.0.0/16 does not belong to 192.168.0.0/17 */
+        if (plen < bl_plen) {
+            continue;
+        }
+
+        if (IN6_IS_ADDR_V4MAPPED(prefix)) {
+            ovs_be32 bl_prefix_v4 = in6_addr_get_mapped_ipv4(&bl_prefix);
+            ovs_be32 prefix_v4 = in6_addr_get_mapped_ipv4(prefix);
+            ovs_be32 mask = be32_prefix_mask(bl_plen);
+
+            if ((prefix_v4 & mask) != (bl_prefix_v4 & mask)) {
+                continue;
+            }
+        } else {
+            struct in6_addr mask = ipv6_create_mask(plen);
+            /* First calculate the difference between bl_prefix and prefix, so
+             * use the bl mask to ensure prefixes are correctly validated.
+             * e.g.: 2005:1734:5678::/50 is a subnet of 2005:1234::/21 */
+            struct in6_addr m_prefixes = ipv6_addr_bitand(prefix, &bl_prefix);
+            struct in6_addr m_prefix = ipv6_addr_bitand(&m_prefixes, &mask);
+            struct in6_addr m_bl_prefix = ipv6_addr_bitand(&bl_prefix, &mask);
+            if (!ipv6_addr_equals(&m_prefix, &m_bl_prefix)) {
+                continue;
+            }
+        }
+        matched = true;
+        break;
+    }
+    free(start);
+    ds_destroy(&filter_list);
+    return matched;
+}
+
+static bool
 prefix_is_deny_listed(const struct smap *nb_options,
                       struct in6_addr *prefix,
                       unsigned int plen)
@@ -1131,7 +1211,9 @@ static bool
 route_need_advertise(const char *policy,
                      struct in6_addr *prefix,
                      unsigned int plen,
-                     const struct smap *nb_options)
+                     const struct smap *nb_options,
+                     const struct nbrec_logical_router *nb_lr,
+                     const struct nbrec_logical_router_port *ts_lrp)
 {
     if (!smap_get_bool(nb_options, "ic-route-adv", false)) {
         return false;
@@ -1153,6 +1235,11 @@ route_need_advertise(const char *policy,
     if (prefix_is_deny_listed(nb_options, prefix, plen)) {
         return false;
     }
+
+    if (!prefix_is_filtered(prefix, plen, nb_lr, ts_lrp, true)) {
+        return false;
+    }
+
     return true;
 }
 
@@ -1207,7 +1294,8 @@ add_static_to_routes_ad(
     const struct nbrec_logical_router *nb_lr,
     const struct lport_addresses *nexthop_addresses,
     const struct smap *nb_options,
-    const char *route_tag)
+    const char *route_tag,
+    const struct nbrec_logical_router_port *ts_lrp)
 {
     struct in6_addr prefix, nexthop;
     unsigned int plen;
@@ -1216,7 +1304,8 @@ add_static_to_routes_ad(
         return;
     }
 
-    if (!route_need_advertise(nb_route->policy, &prefix, plen, nb_options)) {
+    if (!route_need_advertise(nb_route->policy, &prefix, plen, nb_options,
+                              nb_lr, ts_lrp)) {
         return;
     }
 
@@ -1257,7 +1346,8 @@ add_network_to_routes_ad(struct hmap *routes_ad, const char *network,
                          const struct lport_addresses *nexthop_addresses,
                          const struct smap *nb_options,
                          const struct nbrec_logical_router *nb_lr,
-                         const char *route_tag)
+                         const char *route_tag,
+                         const struct nbrec_logical_router_port *ts_lrp)
 {
     struct in6_addr prefix, nexthop;
     unsigned int plen;
@@ -1265,7 +1355,8 @@ add_network_to_routes_ad(struct hmap *routes_ad, const char *network,
         return;
     }
 
-    if (!route_need_advertise(NULL, &prefix, plen, nb_options)) {
+    if (!route_need_advertise(NULL, &prefix, plen, nb_options,
+                              nb_lr, ts_lrp)) {
         VLOG_DBG("Route ad: skip network %s of lrp %s.",
                  network, nb_lrp->name);
         return;
@@ -1319,7 +1410,8 @@ static bool
 route_need_learn(const struct nbrec_logical_router *lr,
                  const struct icsbrec_route *isb_route,
                  struct in6_addr *prefix, unsigned int plen,
-                 const struct smap *nb_options)
+                 const struct smap *nb_options,
+                 const struct nbrec_logical_router_port *ts_lrp)
 {
     if (!smap_get_bool(nb_options, "ic-route-learn", false)) {
         return false;
@@ -1337,6 +1429,11 @@ route_need_learn(const struct nbrec_logical_router *lr,
     if (prefix_is_deny_listed(nb_options, prefix, plen)) {
         return false;
     }
+
+    if (!prefix_is_filtered(prefix, plen, lr, ts_lrp, false)) {
+        return false;
+    }
+
 
     if (route_has_local_gw(lr, isb_route->route_table, isb_route->ip_prefix)) {
         VLOG_DBG("Skip learning %s (rtb:%s) route, as we've got one with "
@@ -1467,7 +1564,7 @@ sync_learned_routes(struct ic_context *ctx,
                 continue;
             }
             if (!route_need_learn(ic_lr->lr, isb_route, &prefix, plen,
-                                  &nb_global->options)) {
+                                  &nb_global->options, lrp)) {
                 continue;
             }
 
@@ -1653,7 +1750,8 @@ build_ts_routes_to_adv(struct ic_context *ctx,
                        struct lport_addresses *ts_port_addrs,
                        const struct nbrec_nb_global *nb_global,
                        const char *ts_route_table,
-                       const char *route_tag)
+                       const char *route_tag,
+                       const struct nbrec_logical_router_port *ts_lrp)
 {
     const struct nbrec_logical_router *lr = ic_lr->lr;
 
@@ -1676,7 +1774,7 @@ build_ts_routes_to_adv(struct ic_context *ctx,
         } else if (!strcmp(ts_route_table, nb_route->route_table)) {
             /* It may be a route to be advertised */
             add_static_to_routes_ad(routes_ad, nb_route, lr, ts_port_addrs,
-                                    &nb_global->options, route_tag);
+                                    &nb_global->options, route_tag, ts_lrp);
         }
     }
 
@@ -1688,7 +1786,7 @@ build_ts_routes_to_adv(struct ic_context *ctx,
                 add_network_to_routes_ad(routes_ad, lrp->networks[j], lrp,
                                          ts_port_addrs,
                                          &nb_global->options,
-                                         lr, route_tag);
+                                         lr, route_tag, ts_lrp);
             }
         } else {
             /* The router port of the TS port is ignored. */
@@ -1752,7 +1850,7 @@ collect_lr_routes(struct ic_context *ctx,
             route_tag = "";
         }
         build_ts_routes_to_adv(ctx, ic_lr, routes_ad, &ts_port_addrs,
-                               nb_global, route_table, route_tag);
+                               nb_global, route_table, route_tag, lrp);
         destroy_lport_addresses(&ts_port_addrs);
     }
 }
